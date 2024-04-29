b9a3e86a4b72c8e7f13541cda219d5b789a8749b
hugorebel1998 <hugorebel1998@gmail.com>
2024-04-29 13:47:56 -0600

    Usuario: Se crea logica de migraciones y su modelo

---
+++ app/Models/Usuario.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    const ESTATUS_ACTIVO   = 'activo';
+    const ESTATUS_INACTIVO = 'inactivo';
+
+    protected $table = 'usuarios';
+
+    protected $fillable = [
+        'nombre',
+        'paterno',
+        'materno',
+        'username',
+        'email',
+        'password',
+        'estatus',
+    ];
+
+
+    protected $appends = [
+        'nombre_completo'
+    ];
+
+    protected $attributes = [
+        'estatus' => self::ESTATUS_ACTIVO
+    ];
+
+    protected $hidden = [
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+
+
+    use HasFactory, HasUlids, SoftDeletes;
+
+    protected function nombreCompleto(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => implode(' ', array_values([$this->nombre, $this->paterno, $this->materno]))
+        );
+    }
+}
